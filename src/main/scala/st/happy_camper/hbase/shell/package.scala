ea4e97d064eb39a126380d0f3b70b9d94fb8fcfc
Takuya UESHIN <ueshin@happy-camper.st>
2012-07-30 01:41:32 +0900

    Update file header.
    Signed-off-by: Takuya UESHIN <ueshin@happy-camper.st>

--- src/main/scala/st/happy_camper/hbase/shell/package.scala
+++ src/main/scala/st/happy_camper/hbase/shell/package.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Happy-Camper Street.
+ * Copyright 2011-2012 Happy-Camper Street.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
